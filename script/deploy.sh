48f4b306f612ed75eb42053e21827f39af0713f4
pinomaker-hoo <inhoo23@naver.com>
2025-04-27 15:47:09 +0900

    fix : Deploy

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,4 +1,4 @@
 echo "Deploy to WAS DEV"
 pnpm build && \
-scp -p 26022 -r ./apps/portfolio-web/dist/* mars:/usr/share/nginx/html/app && \
-scp -p 26022 -r ./apps/wbs-web/dist/* mars:/usr/share/nginx/html/web
+scp -P 26022 -r ./apps/portfolio-web/dist/* mars:/usr/share/nginx/html/app && \
+scp -P 26022 -r ./apps/wbs-web/dist/* mars:/usr/share/nginx/html/web
